5e38e3d5857293dbd0339f9c584767f646bbc0f6
KP Web Dev <kpwebdevishere@gmail.com>
2023-10-16 22:37:32 +0530

    completed the whole UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: "t-",
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        10: "10px",
+        12: "12px",
+        14: "14px",
+        15: "15px",
+        16: "16px",
+        24: "24px",
+        50: "50px",
+      },
+      colors: {
+        home: "#e9e9e9",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        ab: ["ABeeZee", "sans-serif"],
+        os: ["Open Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
